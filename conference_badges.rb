9614911081b2e4d2f5a1e3167a4531b968846117
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-02-25 21:33:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,8 @@ end
 def batch_badge_creator(speakers)
   speakers.map do |speaker|
     "Hello, my name is #{speaker}"
-end
  end
+end
 def assign_rooms(speakers)
   speakers.each_with_index.map do |speaker,index|
   puts "Hello,#{speaker}!You'll be assigned to room #{index+1}!"
