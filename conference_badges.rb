31e55241e95757a131fb1764fd77358813f9be37
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-02-25 21:15:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+array_of_speakers = ["Ada","Charles","Alan","Grace","Linus","Matz"]
+def badge_maker(array_of_speakers)
+  puts "Hello,"
+end
+def batch_badge_creator(array_of_speakers)
+  puts ""
+end
+def assign_rooms(speaker,number)
+  puts "Hello,#{speaker}!You'll be assigned to room #{number}"
+end
