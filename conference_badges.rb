1918329c74974ceee871e443ea2a00ad4fc57e32
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-02-25 21:17:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,9 @@
-array_of_speakers = ["Ada","Charles","Alan","Grace","Linus","Matz"]
-def badge_maker(array_of_speakers)
-  puts "Hello,"
+speakers = ["Ada","Charles","Alan","Grace","Linus","Matz"]
+def badge_maker(name)
+   "Hello,my name is #{name}"
 end
-def batch_badge_creator(array_of_speakers)
-  puts ""
+def batch_badge_creator(speakers)
+  speakers.map do |speaker|
 end
 def assign_rooms(speaker,number)
   puts "Hello,#{speaker}!You'll be assigned to room #{number}"
